30a0ae0909c1362ef992e09003bbe6a0c4b9cc03
francisco <franciscoolivaresrivera@gmail.com>
2019-09-26 13:45:48 -0300

    Correccion nombres BDD y Tabla

--- Mysql_db_table.sql
+++ Mysql_db_table.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE `maven_spring2` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-CREATE TABLE `registros2` (
+CREATE DATABASE `maven_spring` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `registros` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `valor` int(11) NOT NULL,
